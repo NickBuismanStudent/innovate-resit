8163998a4b2cc3d8fafd341ace21053b05c801a5
NickBuismanStudent <nick.buisman@student.nhlstenden.com>
2022-07-13 22:27:01 +0200

    First steps in Console app
    Added a default template to the gitignore and uploaded the project files

---
+++ ConsoleApp/Meeting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CubicSetupApp
+{
+    internal class Meeting
+    {
+        public Meeting(string title, string description, string location, string date, string timeBegin, string timeEnd)
+        {
+            this.title = title;
+            this.description = description;
+            this.location = location;
+            this.date = date;
+            this.timeBegin = timeBegin;
+            this.timeEnd = timeEnd;
+        }
+        public string title { get; set; }
+        public string location { get; set; }
+        public string description { get; set; }
+        public string date { get; set; }
+        public string timeBegin { get; set; }
+        public string timeEnd { get; set; }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CubicSetupApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = CreateJson();
+            Console.WriteLine(test);
+            Console.ReadLine();
+        }
+
+        static string CreateJson()
+        {
+            Console.WriteLine("Please enter a title for the meeitng");
+            string Title = Console.ReadLine();
+            Console.WriteLine("Please enter a description for the meeting");
+            string Description = Console.ReadLine();
+            string Location = Console.ReadLine();
+            string date = Console.ReadLine();
+            string timeStart = Console.ReadLine();
+            string timeEnd = Console.ReadLine();
+            Meeting Meeting = new Meeting(Title, Description, Location, date, timeStart, timeEnd);
+            string JsonString = JsonSerializer.Serialize(Meeting);
+            return JsonString;
+        }
+    }
+}
