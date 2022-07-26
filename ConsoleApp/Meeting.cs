fd4651fe6bdf70f26b43b99d47f6bb6e2404f165
NickBuismanStudent <nick.buisman@student.nhlstenden.com>
2022-07-26 21:10:54 +0200

    Reorganised functions
    reorganised the functions for adding a meeting.

--- ConsoleApp/Meeting.cs
+++ ConsoleApp/Meeting.cs
@@ -9,20 +9,18 @@ namespace CubicSetupApp
 {
     internal class Meeting
     {
-        public Meeting(string title, string description, string location, string date, string timeBegin, string timeEnd)
+        public Meeting(string title, string description, string location, string date, string timeBegin)
         {
             this.title = title;
             this.description = description;
             this.location = location;
             this.date = date;
             this.timeBegin = timeBegin;
-            this.timeEnd = timeEnd;
         }
         public string title { get; set; }
         public string description { get; set; }
         public string location { get; set; }
         public string date { get; set; }
         public string timeBegin { get; set; }
-        public string timeEnd { get; set; }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Text;
 using System.Text.Json;
+using System.Net;
 using System.Net.Http;
 
 
@@ -11,17 +12,85 @@ namespace CubicSetupApp
 {
     internal class Program
     {
+        // Public HTTP Client to allow api calls from all methods
+        public static HttpClient client { get; set; } = new HttpClient();
+
         static void Main(string[] args)
         {
-            string test = CreateJson();
-            Console.WriteLine(test);
-            SendJson(test);
+            Console.WriteLine("What is the CubicCalendar's IP address?");
+            string address = SetAddress(); 
+            client.BaseAddress = new Uri(@"http://" + address + @":8000");
+            Console.WriteLine("What would you like to do? \nAdd a meeting (add)\nEdit a meeting (edit)\nRemove a meeting (remove)");
+            commands();
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// Asks the user for a command to execute.
+        /// If the command is not recognised it will recursively ask again
+        /// 
+        /// 
+        /// </summary>
+        static void commands()
+        {
+            string cmd = Console.ReadLine();
+            cmd = cmd.ToLower();
+            switch (cmd)
+            {
+                case "add":
+                    string meeting = CreateJson();
+                    SendJson(meeting);
+                    commands();
+                    break;
+                case "edit":
+                    Console.WriteLine("sorry, not implemented yet");
+                    commands();
+                    break;
+                case "remove":
+                    Console.WriteLine("sorry, not implemented yet");
+                    commands();
+                    break;
+                default:
+                    Console.WriteLine("I don't recognise {0}, Please try again", cmd);
+                    commands();
+                    break;
+            }
+        }
+        
+        /// <summary>
+        /// Sends a TTS command to the CubicCalendar
+        /// </summary>
+        /// <param name="text">Content of the message</param>
+        static void tts(string text)
+        {
+            client.GetAsync("alert/"+text);
+        }
+
+        /// <summary>
+        /// Recursive function that reads and validates an IP from the console.
+        /// </summary>
+        /// <returns>a validated IP in string format</returns>
+        static string SetAddress()
+        {
+            string input = Console.ReadLine();
+
+            IPAddress ip;
+            if (IPAddress.TryParse(input, out ip))
+            {
+                return ip.ToString();
+            }
+            Console.WriteLine("Please enter a valid IP");
+            return SetAddress();
+
+        }
+
+        /// <summary>
+        /// Creates a Json file for adding a meeting
+        /// </summary>
+        /// <returns>Json serialised string with meeting details</returns>
         static string CreateJson()
         {
-            Console.WriteLine("Please enter a title for the meeitng");
+            Console.WriteLine("Please enter a title for the meeting");
             string Title = Console.ReadLine();
             Console.WriteLine("Please enter a description for the meeting");
             string Description = Console.ReadLine();
@@ -31,21 +100,29 @@ namespace CubicSetupApp
             string date = Console.ReadLine();
             Console.WriteLine("Please enter a starting time for the meeting");
             string timeStart = Console.ReadLine();
-            Console.WriteLine("Please enter the end time for the meeting");
-            string timeEnd = Console.ReadLine();
-            Meeting Meeting = new Meeting(Title, Description, Location, date, timeStart, timeEnd);
+            Meeting Meeting = new Meeting(Title, Description, Location, date, timeStart);
             string JsonString = JsonSerializer.Serialize(Meeting);
             return JsonString;
         }
 
-        static string SendJson(string json)
+        /// <summary>
+        /// Sends the JSON string to the cube api
+        /// </summary>
+        /// <param name="json"> Json serialised string with meeting details</param>
+        /// <param name="address"> IP address string </param>
+        /// <returns></returns>
+        static void SendJson(string json)
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(@"http://127.0.0.1:8000/");
             var content = new StringContent(json);
-            var result = client.PostAsync("calendar/add", content).Result;
-            Console.WriteLine(result);
-            return "Success";
+            try
+            {
+                var result = client.PostAsync("calendar/add", content).Result;
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
